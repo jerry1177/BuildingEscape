2823d6d733870df0596a1eb8a0801acd0fc93e8f
DESKTOP-0A2LIK0\jerry <jerrymaldonadom@gmail.com>
2018-08-26 20:15:39 -0700

    First room geometry built

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-#include "GameFramework/Actor.h"
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
